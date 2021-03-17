4d879da913426c8528a4c824ab25b2abfb8018c5
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-17 10:13:21 -0300

    Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros. Extra exibindo em km, hm, dam, dm, cm e mm.

--- .idea/exercicio08.py
+++ .idea/exercicio08.py
@@ -1,1 +1,8 @@
-distancia = float(input(''))
+d = float(input('Digite uma distancia em metros: '))
+km = d/1000
+hm = d/100
+dam = d/10
+dm = d*10
+cm = d*100
+mm = d*1000
+print('A medida de {} metros corresponde a \n{}km \n{}hm \n{}dam \n{}dm \n{}cm \n{}mm'.format(d, km, hm, dam, dm, cm, mm))

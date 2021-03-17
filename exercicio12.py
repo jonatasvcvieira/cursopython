c287c99daf586f0630ebf9f5d3de08b926f21fa9
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-17 15:27:10 -0300

    Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.

--- exercicio12.py
+++ exercicio12.py
@@ -1,3 +1,3 @@
 prod = float(input('Digite o valor do produto:'))
-promo = (prod)-(prod * 0.05)
+promo = prod - (prod * 5 / 100)
 print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format(prod, promo))

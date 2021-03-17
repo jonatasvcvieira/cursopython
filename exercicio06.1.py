5c6696c6839a5ebd3d16058d7270d6637818cee0
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-17 09:20:56 -0300

    Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada. Em outra formatação.

---
+++ exercicio06.1.py
@@ -0,0 +1,2 @@
+n = int(input('Digite um número: '))
+print('O dobro de {} vale {}. \nO triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.4f}.'.format(n, (n*2), n, (n*3), n, pow(n, (1/2))))

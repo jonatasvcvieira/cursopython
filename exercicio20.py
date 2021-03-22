8aba4fd5b7943e7d205db8ba18eb05828a681af2
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-22 16:46:44 -0300

    Sorteando uma ordem na lista

---
+++ exercicio20.py
@@ -0,0 +1,22 @@
+'''O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos.
+Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+'''
+
+'''
+import random
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem de apresentação será {}'.format(lista))
+'''
+from random import shuffle
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1, n2, n3, n4]
+shuffle(lista)
+print('A ordem de apresentação será {}'.format(lista))

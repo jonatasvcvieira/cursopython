841fd3f1f8adc1514b31d70d19a015bd87bd7d43
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-22 17:12:07 -0300

    Tocando um MP3

---
+++ exercicio21.py
@@ -0,0 +1,14 @@
+'''
+Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+'''
+
+'''
+import pygame
+pygame.init()
+pygame.mixer.music.load('exe21jazz.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+'''
+
+
+

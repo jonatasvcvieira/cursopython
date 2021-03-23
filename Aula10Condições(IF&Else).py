9914131ccfe3101ec047908fc54045d99762251b
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-23 12:09:03 -0300

    Nessa aula, vamos aprender como utilizar estruturas condicionais simples e compostas nos seus programas em Python.

---
+++ Aula10Condições(IF&Else).py
@@ -0,0 +1,41 @@
+'''
+Condições, If or Else
+'''
+
+'''
+tempo = int(input('Qunatos anos tem seu carro?'))
+if tempo<=3:
+    print('Seu carro é seminovo')
+else:
+    print('Seu carro é usado')
+print('--Fim--')
+'''
+
+'''
+tempo = int(input('Quantos anos tem seu carro?'))
+print('Seu carro é seminovo.'if tempo<=3 else'Seu carro é usado.')
+print('---Fim---')
+'''
+'''
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é tão normal!')
+print('Bom dia, {}'.format(nome))
+'''
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1+n2)/2
+print('A sua média foi {:.1f}'.format(m))
+if m>= 6.0:
+    print('Sua média foi boa" PARABÉNS!')
+else:
+    print('Sua média foi ruim! ESTUDE MAIS!')
+'''
+Simplificada
+#print('PARABÉNS' if m >=6 else 'ESTUDE MAIS')
+'''
+
+

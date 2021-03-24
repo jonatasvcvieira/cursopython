2927003d4cb39b52360e307c11cb5edad0938eb4
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-24 16:16:45 -0300

    Conversor de Bases Numéricas

--- exercicio37.py
+++ exercicio37.py
@@ -11,11 +11,11 @@ print('''Escolha uma das bases número inteiro:
 [ 3 ] Converter para Hexadecimal''')
 opção = int(input('Sua opção: '))
 if opção == 1:
-    print('{} convertido para Binário é igual a {}'.format(num, bin(num)))
+    print('{} convertido para Binário é igual a {}'.format(num, bin(num)[2:]))
 elif opção == 2:
-    print('{} convertido para Octal é igual a {}'.format(num, oct(num)))
+    print('{} convertido para Octal é igual a {}'.format(num, oct(num)[2:]))
 elif opção == 3:
-    print('{} convertido para Hexadecimal é igual a {}'.format(num, hex(num)))
+    print('{} convertido para Hexadecimal é igual a {}'.format(num, hex(num)[2:]))
 else:
     print('Opção inválida. Tente novamente.')
 

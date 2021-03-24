e74ddb640f31422d1fabf3bcbbd3043c3fde2cde
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-24 18:28:28 -0300

    Classificando Atletas

--- exercicio41.py
+++ exercicio41.py
@@ -13,3 +13,16 @@ de acordo com a idade:
 – Acima de 25 anos: MASTER
 '''
 
+from datetime import date
+atual = date.today().year
+nascimento = int(input('Ano de nascimento:'))
+idade = atual - nascimento
+print('O atleta tem {} anos.'.format(idade))
+if idade <= 9:
+    print('Classificação: Mirim')
+elif idade <= 14:
+    print('Classificação: Junior')
+elif idade <= 25:
+    print('Classificação: Sênior')
+else:
+    print('Classificação: Master')

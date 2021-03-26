5f23fb4c12b4b77cc1e8498bebd874e40725700b
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-26 09:59:06 -0300

    Índice de Massa Corporal

--- exercicio43.py
+++ exercicio43.py
@@ -12,4 +12,18 @@ Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Ín
 – Acima de 40: Obesidade Mórbida
 '''
 
-peso = float(input('Qual é o seu peso: {Kg} '))
+peso = float(input('Qual é o seu peso? {Kg} '))
+altura = float(input('Qual é o sua altura? (m) '))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você está abaixo do peso normal')
+elif 18.5 <= imc < 25:
+    print('Parabéns você está na faixa de peso normal')
+elif 25 <= imc <30:
+    print('Você está em sobrepeso')
+elif 30 <= imc <40:
+    print('Você está em obesidade')
+elif imc >= 40:
+    print('Você está em obesidade mórbida')
+

a5944689f3b59428b3e94dc480da79714fa232d3
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-26 16:17:06 -0300

    Progressão Aritmética

---
+++ exercicio51.py
@@ -0,0 +1,9 @@
+'''
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+'''
+primeiro = int(input('Digite o primeiro termo:'))
+razão = int(input('Razão:'))
+décimo = primeiro + (10 - 1) * razão
+for c in range(primeiro, décimo + razão, razão ):
+    print('{}'.format(c), end='- ')
+print('Acabou')

7b1ccb74b8899f465e0bcf843e6b2c5b8a0e0513
jvasconcelosdev <jonatasvieir@gmail.com>
2021-04-05 09:33:31 -0300

    Super Progressão Aritmética v3.0

---
+++ exercicio62.py
@@ -0,0 +1,18 @@
+print('Gerador de PA')
+print('-=-' * 5)
+primeiro = int(input('Primeiro termo:'))
+razão = int(input('Razão da PA:'))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} -> '.format(termo), end='')
+        termo += razão
+        cont += 1
+    print('Pausa')
+    mais = int(input('Quantos termos você quer mostrar a mais?'))
+print('Fim!')
+print('Progressão finalizada com {} termos mostrados.'.format(total))

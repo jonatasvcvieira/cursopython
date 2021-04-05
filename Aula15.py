701864ea3f1535762ee6efbcef169c3750893790
jvasconcelosdev <jonatasvieir@gmail.com>
2021-04-05 16:42:29 -0300

    Interrompendo repetições while

---
+++ Aula15.py
@@ -0,0 +1,14 @@
+'''n = s = 0
+while True:
+    n = int(input('Digite um número:'))
+    if n == 999:
+        break
+    s += n
+# print('A soma vale {}'.format(s))
+print(f'A soma vale {s}')'''
+
+nome = 'Jose'
+idade = 33
+salário = 987.3
+print(f'O {nome} tem {idade} anos e ganha R${salário:.2f}')
+

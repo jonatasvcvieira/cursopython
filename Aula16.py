3cea94f16efc6a475c9824b28c3e7c6e3a6f0475
jvasconcelosdev <jonatasvieir@gmail.com>
2021-04-23 18:22:06 -0300

    Tuplas

---
+++ Aula16.py
@@ -0,0 +1,53 @@
+#lanche = ('Hambúrger', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+'''
+print(lanche)
+print(lanche[0])
+print(lanche[3])
+print(lanche[-2])
+print(lanche[1:3])
+print(lanche[2:])
+print(lanche[:2])
+'''
+
+'''
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+'''
+
+#print(len(lanche))
+
+
+#for cont in range(0, len(lanche)):
+#    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+
+#for comida in lanche:
+#    print(f'Eu vou comer {comida}')
+
+
+#print(len(lanche))
+
+#for pos, comida in enumerate(lanche):
+#    print(f'Eu vou comer {comida} na posição {pos}')
+
+#print('Comi pra caramba!')
+
+#print(sorted(lanche))
+#print(lanche)
+'''
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+print(c)
+print(len(c))
+print(c.count(5))
+print(c)
+print(c.index(8))
+'''
+
+pessoa = ('Gustavo', 39, 'M', 99.88)
+del(pessoa)
+print(pessoa)
+
+
+

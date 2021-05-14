220236e4ccbbd14c304ec5daf306f5a2dc274a19
jvasconcelosdev <jonatasvieir@gmail.com>
2021-05-14 19:47:03 -0300

    Boletim com listas compostas

---
+++ exercicio89.py
@@ -0,0 +1,25 @@
+ficha = list()
+while True:
+    nome = str(input('Digite o nome do aluno:'))
+    nota1 = float(input('Digite a primeira nota:'))
+    nota2 = float(input('Digite a segunda nota:'))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continuar? [S/N]'))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-' * 26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 30)
+    opc = int(input('Mostrar as notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<< VOLTE SEMPRE >>>')
+

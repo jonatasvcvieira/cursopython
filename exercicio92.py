7f66d629eb4af06431d6ebc3ea1bee7239b5cce6
jvasconcelosdev <jonatasvieir@gmail.com>
2021-05-19 16:02:43 -0300

    Cadastro de Trabalhador em Python

---
+++ exercicio92.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+dados = dict()
+dados['nome'] = str(input('Nome:'))
+nasc = int(input('Ano do Nascimento:'))
+dados['idade'] = datetime.now().year - nasc
+dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if dados ['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de contratação:'))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+print('-=' * 20)
+for k, v in dados.items():
+    print(f'   - {k} tem o valor {v}')
+

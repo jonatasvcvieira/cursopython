462dbe928288ebfa17fc67739baff166566573cb
jvasconcelosdev <jonatasvieir@gmail.com>
2021-05-19 10:49:36 -0300

    Dicionários em Python

---
+++ exercicio90.py
@@ -0,0 +1,13 @@
+#aluno = {}
+aluno = dict()
+aluno['Nome'] = str(input('Nome:'))
+aluno['Média'] = float(input(f'Média de {aluno["Nome"]}:'))
+if aluno['Média'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+elif 5 <= aluno['Média'] <7:
+    aluno['Situação'] = 'Recuperação'
+else:
+    aluno['Situação'] = 'Reprovado'
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f'   - {k} é igual a {v}.')

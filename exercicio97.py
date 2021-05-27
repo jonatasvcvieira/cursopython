02796bf9ac72c6d5155e3083789037c0cdb45963
jvasconcelosdev <jonatasvieir@gmail.com>
2021-05-27 12:25:36 -0300

    Um print especial

---
+++ exercicio97.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+#Programa Principal
+escreva('Gustavo Guanabara')
+escreva('Curso de Python3 no YouTube')
+escreva('Curso em Video')

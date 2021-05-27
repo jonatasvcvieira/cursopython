097c55ebf4f5775a6e7137d5f53aed4c1f6b0ed6
jvasconcelosdev <jonatasvieir@gmail.com>
2021-05-27 12:06:31 -0300

    Função que calcula área

---
+++ exercicio96.py
@@ -0,0 +1,12 @@
+def área(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno {larg} x {comp} é de {a}m².')
+
+
+#Programa principal
+print(' Controle de Terrenos')
+print('-' * 20)
+l = float(input('Largura (m):'))
+c = float(input('Comprimento (m):'))
+área(l, c)
+

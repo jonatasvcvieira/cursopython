0a7f0c3f535d0642cc56a1bd195b27dc1307b5cc
jvasconcelosdev <jonatasvieir@gmail.com>
2021-05-27 17:56:54 -0300

    Funções (Parte 2)
    # Interactive Help
    # Docstrings
    # Argumentos opcionais
    # Escopo de variáveis
    # Retorno de resultados

--- Aula21.py
+++ Aula21.py
@@ -96,6 +96,7 @@ print(f'A fora vale {a}')
 
 # ###### Retorno de resultados.
 
+'''
 def somar(a=0, b=0, c=0):
     s = a + b + c
     return s
@@ -105,4 +106,33 @@ r2 = somar(2, 2)
 r3 = somar(6)
 
 print(f'Os resultados foram {r1}, {r2}, {r3}')
+'''
 
+# Pratica da Aula
+
+'''
+def fatorial(num=1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+print(f'Os resultados são {f1}, {f2} e {f3}')
+'''
+
+def par(n=0):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+num = int(input('Digite um número:'))
+if par(num):
+    print('É par!')
+else:
+    print('Não é par!')

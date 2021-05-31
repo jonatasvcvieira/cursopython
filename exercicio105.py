6a09f00d2a78976a8a9fb913342aff9ac5e57206
jvasconcelosdev <jonatasvieir@gmail.com>
2021-05-31 17:12:05 -0300

    Analisando e gerando Dicionários

---
+++ exercicio105.py
@@ -0,0 +1,29 @@
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos (aceita várias)
+    :param sit: valor opcional, indicando se deve ou não adcionar a situação
+    :return: dicionário com várias informações sobre a situação da turma.
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['manor'] = min(n)
+    r['média'] = sum(n)/len(n)
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'Boa'
+        elif r['média'] >= 5:
+            r['situação'] = 'Razoável'
+        else:
+            r['situação'] = 'Ruim'
+        return r
+
+
+# Programa principal
+resp = notas(5.5, 2.5, 1.5, sit=True)
+print(resp)
+print()
+print('<->' * 30)
+print()
+help(notas)

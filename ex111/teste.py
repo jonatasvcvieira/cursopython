a67eb90df4b4e6bbdf859c4d2781cf3d6a25bec7
jvasconcelosdev <jonatasvieir@gmail.com>
2021-06-02 10:25:33 -0300

    Exercitando módulos em Python

---
+++ ex111/teste.py
@@ -0,0 +1,12 @@
+from ex111.utilidadescev import moeda
+
+p = float(input('Digite o preço: R$ '))
+#moeda.resumo(p)
+moeda.resumo(p, 20, 12)
+
+'''
+print(f'A metade de R${moeda.moeda(p)} é {moeda.metade(p, True)}')
+print(f'O dobro de R${moeda.moeda(p)} é {moeda.dobro(p, True)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(p, 10, True)}')
+print(f'Reduzindo 10%, temos {moeda.diminuir(p, 10, True)}')
+'''
